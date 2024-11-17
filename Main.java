fd24cce4461fc74c344216e15eee632dcfc13b32
Patrik Holba <paveegamedev@gmail.com>
2024-11-17 12:25:57 -0500

    Questions

---
+++ Main.java
@@ -0,0 +1,51 @@
+public class Main {
+
+    // !IMPORTANT - If you're reading this in a browser, you're doing something wrong.
+    // Find you're code editor and continue there
+
+
+    //You made it here, now just answer the questions and you're almost done.
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+        //Question 1 - Create a variable with your name and output on a separate line (find how to do it) - "Hello: " and your name
+
+
+
+        //Question 2 - Good job on completing your first question, now output the same thing 10 times.
+
+
+
+
+        //Question 3 - Good job on completing your second question, now output the same thing 10 thousand times.
+
+
+
+
+
+        //Question 4 - I hope you weren't writing it down 10 thousand times and that you found a better solution, now output the same thing only if the name exactly matches yours, otherwise output NO (if I change the variable to Patrik, it will output NO).
+
+
+
+
+
+
+        //Question 4 - I hope you weren't writing it down 10 thousand times and that you found a better solution, now output the same thing only if the name exactly matches yours, otherwise output NO (if I change the variable to Patrik, it will output NO).
+
+
+
+
+
+
+        //Question 5 - Find a way to find if a number is even or odd and output it for 1 000 numbers. (from 0 to 999)
+
+
+
+
+
+
+        //Good job, now just submit this assignment if you know how :).
+
+    }
+}
